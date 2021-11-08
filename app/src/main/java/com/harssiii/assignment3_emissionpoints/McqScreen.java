f8b61e97e12890f434e8f9ec79fb38844e075166
Harssiii <m.harry390@gmail.com>
2021-11-08 10:55:09 -0800

    Image,Intent And Actvity

---
+++ app/src/main/java/com/harssiii/assignment3_emissionpoints/McqScreen.java
@@ -0,0 +1,24 @@
+package com.harssiii.assignment3_emissionpoints;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class McqScreen<message> extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mcq_screen);
+    }
+    // Get the Intent that started this activity and extract the string
+    Intent intent = getIntent();
+    String message = intent.getStringExtra(StartPage.EXTRA_MESSAGE);
+    String message2 = intent.getStringExtra(StartPage.EXTRA_MESSAGE);
+
+    // Capture the layout's TextView and set the string as its text
+    TextView textView2 = findViewById(R.id.textView2);
+//    textView2.setText(message)
+}
--- app/src/main/java/com/harssiii/assignment3_emissionpoints/StartPage.java
+++ app/src/main/java/com/harssiii/assignment3_emissionpoints/StartPage.java
@@ -2,13 +2,28 @@ package com.harssiii.assignment3_emissionpoints;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class StartPage extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE ="" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_page);
+    }
+    public void startQuiz(View view) {
+        Intent intent = new Intent(this, McqScreen.class);
+        EditText editText = (EditText) findViewById(R.id.PersonName);
+        EditText editText2 = (EditText) findViewById(R.id.EmailAddress);
+        String message = editText.getText().toString();
+        String message2 = editText2.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_MESSAGE, message2);
+        startActivity(intent);
     }
 }
