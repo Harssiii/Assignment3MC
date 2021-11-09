e9681b09fc98d8e12baa9afd9d9fafd480d2ca61
Harssiii <m.harry390@gmail.com>
2021-11-08 23:14:29 -0800

    GitLink Added

--- app/src/main/java/com/harssiii/assignment3_emissionpoints/MainActivity.java
+++ app/src/main/java/com/harssiii/assignment3_emissionpoints/MainActivity.java
@@ -4,18 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /** Called when the user taps the Send button */
+        TextView noteView = (TextView) findViewById(R.id.noteView);
+        noteView.setText("https://github.com/Harssiii/Assignment3MC");
+        Linkify.addLinks(noteView, Linkify.WEB_URLS);
 
     }
+
 
 
     public void startapp(View view) {
