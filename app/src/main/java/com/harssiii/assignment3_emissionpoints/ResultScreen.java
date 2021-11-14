1448f99afc0fb5530bf55b26f13cbeb4b3d08b39
Harssiii <m.harry390@gmail.com>
2021-11-13 19:52:03 -0800

    Share Button Implemented

--- app/src/main/java/com/harssiii/assignment3_emissionpoints/ResultScreen.java
+++ app/src/main/java/com/harssiii/assignment3_emissionpoints/ResultScreen.java
@@ -33,13 +33,29 @@ public class ResultScreen extends AppCompatActivity {
         tv10.setText(sb);
         tv11.setText(sb2);
         tv12.setText(sb3);
+        btnShare.setOnClickListener(this::onClick);
 
-        McqScreen.correct=0;
-        McqScreen.wrong=0;
+
+
 
         btnRestart.setOnClickListener(v -> {
+            McqScreen.correct=0;
+            McqScreen.wrong=0;
             Intent in = new Intent(getApplicationContext(),MainActivity.class);
             startActivity(in);
         });
     }
+
+
+    private void onClick(View view) {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "Quiz Score Is" + " " + McqScreen.correct +"0"  ;
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Share via"));
+
+    }
+
+
 }
